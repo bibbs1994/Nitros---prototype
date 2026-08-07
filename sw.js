406f66a180ed999ad2fecd141e4d90e891fe2543
bibbs1994 <bibbs1994@gmail.com>
2026-08-07 07:11:03 -0400

    build v10.12.7AC

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const VERSION = '10.12.7AB';
+const VERSION = '10.12.7AC';
 const CACHE_PREFIX = 'nitros-mobile-technician-portal-';
 const CACHE_NAME = `${CACHE_PREFIX}${VERSION}`;
+const OBSOLETE_CLASSIFIER_CACHE_PREFIXES = [CACHE_PREFIX, 'nitros-image-classifier-', 'nitros-classifier-'];
 const APP_SHELL = new URL('./index.html', self.registration.scope).href;
 
 self.addEventListener('install', event => {
@@ -12,7 +13,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil((async () => {
     const names = await caches.keys();
-    await Promise.all(names.filter(name => name.startsWith(CACHE_PREFIX) && name !== CACHE_NAME).map(name => caches.delete(name)));
+    await Promise.all(names.filter(name => name !== CACHE_NAME && OBSOLETE_CLASSIFIER_CACHE_PREFIXES.some(prefix => name.startsWith(prefix))).map(name => caches.delete(name)));
     await self.clients.claim();
   })());
 });
